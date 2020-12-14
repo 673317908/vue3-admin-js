4f432547a350679c035c6a08637adae40ec5d3bf
673317908Liyi <liy@sayingdata.com>
2020-12-14 18:00:20 +0800

    制作登录页、添加阿里云滑动验证

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
-import { createApp } from "vue";
+import {
+  createApp
+} from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
+// 引入ant
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+
 createApp(App)
   .use(store)
   .use(router)
+  .use(Antd)
   .mount("#app");
