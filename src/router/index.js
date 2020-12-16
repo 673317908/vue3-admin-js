1f035bb904c9d5fd6ee836d32be6c3e9265db0a6
673317908Liyi <liy@sayingdata.com>
2020-12-16 13:30:28 +0800

    路由添加

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,27 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import {
+  createRouter,
+  createWebHashHistory
+} from "vue-router";
 
-const routes = [
-  {
-    path: "/",
+const routes = [{
+    path: "/login",
     name: "Login",
-    component: ()=>import("../views/acount/login.vue")
+    component: () => import("../views/acount/login.vue")
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: () => import("../views/acount/register.vue")
+  },
+  {
+    path: "/forget",
+    name: "Forget",
+    component: () => import("../views/acount/forget.vue")
+  },
+  {
+    path: "/index",
+    name: "Index",
+    component: () => import("../views/layout/index.vue")
   },
 ];
 
