90d0dbc46e6351432254a55687fc4997870c86f4
673317908Liyi <liy@sayingdata.com>
2020-12-16 13:30:58 +0800

    后台首页创建

---
+++ src/views/layout/Aside.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>菜单</div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  created() {},
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style scoped lang="less"></style>
---
+++ src/views/layout/Main.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>内容</div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  created() {},
+  mounted() {},
+  methods: {},
+};
+</script>
+
+<style scoped lang="less"></style>
